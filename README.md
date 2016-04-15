d930a518774944eb83fcd734ce17a3ef1bd897ee
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-04-15 22:55:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,107 @@
+Megumi
+======
+
+![Mwgumi](http://49.media.tumblr.com/a66096a9f30db85c28292931e4860c4e/tumblr_nfagc7sUIv1tlyjpto1_500.gif)
+
+> <a href="http://hheiyu.tumblr.com/post/103040653078" target="_blank">I have a thing for ohohojousama</a>
+
+# Usage
+
+```JavaScript
+
+const Megumi = require('megumi');
+const twitter_keys = require('./twitter.json');
+
+console.log(twitter_keys);
+/** =>
+{
+  "consumer_key":         "...",
+  "consumer_secret":      "...",
+  "access_token":         "...",
+  "access_token_secret":  "..."
+}
+ */
+
+const megumi = new Megumi(twitter_keys);
+
+const opts = {
+  screen_name: 'shiratamacaron',
+  sort: 'sum',
+  range: 'last_month',
+  num: 12,
+};
+
+megumi.retrieve(opts).then( tweets => {
+
+  console.log(tweets.length);
+  //=> 12
+
+  console.log(tweets[0]);
+  /** =>
+  { created_at: 'Wed Mar 16 11:54:21 +0000 2016',
+    id: 710071702722490400,
+    id_str: '710071702722490368',
+    text: '初描きにこちゃん　そのうち塗りたい…！ https://t.co/9A7yIAqe1Y',
+    entities:
+     { hashtags: [],
+       symbols: [],
+       user_mentions: [],
+       urls: [],
+       media: [ [Object] ] },
+    extended_entities: { media: [ [Object] ] },
+    :
+    :
+   */
+
+  console.log(tweets[1]);
+  /** =>
+  { created_at: 'Sun Mar 20 15:56:56 +0000 2016',
+  id: 711582301956603900,
+  id_str: '711582301956603904',
+  text: '久々のアナログ にっこり…！(^o^) https://t.co/v6m10vFDYR',
+  entities:
+   { hashtags: [],
+     symbols: [],
+     user_mentions: [],
+     urls: [],
+     media: [ [Object] ] },
+  extended_entities: { media: [ [Object] ] },
+  :
+  :
+  */
+
+});
+
+
+```
+
+# Parameter
+
+### sort
+
+- favorite
+- retweet
+- sum(favorite + retweet)
+
+default is `sum`
+
+### range
+
+- `all`
+- `0`
+-- or `today`
+- `-1`
+-- or `yesterday`
+- `-7`
+-- or `last_week`
+- `-30`
+-- or `last_month`
+- and more(`0` - `-3650`)
+
+default is `all`
+
+### num
+
+- 0 - 3200
+
+default is `12`
---
+++ test/normal.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const _            = require('lodash');
+const assert       = require('power-assert');
+const Megumi       = require('../');
+
+describe('Megumi Normal', () => {
+
+  before( done => {
+    const twitterKeys = process.env.TRAVIS ? process.env : require('../twitter.json');
+    console.log(twitterKeys);
+    this.megumi = new Megumi(twitterKeys);
+    done();
+  });
+
+  it('should return tweets when pass today', (done) => {
+    const opts = {
+      screen_name: 'shiratamacaron',
+      sort: 'favorite',
+      range: 'today',
+      num: 12,
+    };
+
+    this.megumi.retrieve(opts).then( tweets => {
+      assert(_.isArray(tweets));
+      assert(tweets.length <= 12);
+      done();
+    });
+  });
+
+  it('should return tweets when pass yesterday', (done) => {
+    const opts = {
+      screen_name: 'shiratamacaron',
+      sort: 'sum',
+      range: 'yesterday',
+      num: 12,
+    };
+
+    this.megumi.retrieve(opts).then( tweets => {
+      assert(_.isArray(tweets));
+      assert(tweets.length <= 12);
+      done();
+    });
+  });
+
+  it('should return tweets when pass last_week', (done) => {
+    const opts = {
+      screen_name: 'shiratamacaron',
+      sort: 'retweet',
+      range: 'last_week',
+      num: 12,
+    };
+
+    this.megumi.retrieve(opts).then( tweets => {
+      assert(_.isArray(tweets));
+      assert(tweets.length <= 12);
+      done();
+    });
+  });
+
+  it('should return tweets when pass last_month', (done) => {
+    const opts = {
+      screen_name: 'shiratamacaron',
+      sort: 'sum',
+      range: 'last_month',
+      num: 12,
+    };
+
+    this.megumi.retrieve(opts).then( tweets => {
+      assert(_.isArray(tweets));
+      assert(tweets.length <= 12);
+      done();
+    });
+  });
+
+  it('should return tweets when dont pass range paramater', (done) => {
+    const opts = {
+      screen_name: 'shiratamacaron',
+      sort: 'sum',
+      num: 12,
+    };
+
+    this.megumi.retrieve(opts).then( tweets => {
+      assert(_.isArray(tweets));
+      assert(tweets.length <= 12);
+      console.log(tweets[0]);
+      console.log(tweets[1]);
+      done();
+    });
+  });
+});
