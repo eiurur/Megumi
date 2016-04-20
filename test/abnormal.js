25bfb7eddbfa6dbf03952dacba22a059c0a53dbb
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-04-21 00:55:20 +0900

    [Fix] - borken when pass non-exist-user-id_str + refactoring

--- test/abnormal.js
+++ test/abnormal.js
@@ -1,56 +1,35 @@
-// 'use strict';
+'use strict';
 
-// const _       = require('lodash');
-// const assert  = require('power-assert');
-// const Megumi = require('../');
+const _                            = require('lodash');
+const assert                       = require('power-assert');
+const Megumi                       = require('../');
+const TwitterUtil                  = require('../lib/TwitterUtil');
+const NON_EXIST_USER_ERROR_MESSAGE = 'Non-exist user';
 
-// const DATA_LENGTH   = 100;
-// const TASK_ERROR_MESSAGE = 'task_failure';
-// const PARAMETER_IS_INVALID_ERROR_MESSAGE = 'params is invalid';
+describe('Megumi Abnormal', () => {
+  before( done => {
+    const twitterKeys = process.env.TRAVIS ? process.env : require('../twitter.json');
+    this.megumi = new Megumi(twitterKeys);
+    this.isAllTweetHasImage = tweets => tweets.every(TwitterUtil.hasMedia);
+    done();
+  });
 
-// describe('Megumi Abnormal', () => {
-//   beforeEach( (done) => {
-//     this.dataList = Array.from(Array(DATA_LENGTH).keys()); // [0..99];
-//     this.task_failure = a => {
-//       return new Promise( (resolve, reject) => {
-//         throw new Error(TASK_ERROR_MESSAGE);
-//       });
-//     };
-//     done();
-//   });
+  it('should return Error when pass non-exist user', (done) => {
+    const opts = {
+      screen_name: 'Ujimushi_Unko',
+      sort: 'favorite',
+      range: 'today',
+      num: 12,
+    };
 
-//   it('should return Promise.reject when is thrown new Error()', (done) => {
-//     const params = {
-//       skip: 0,
-//       limit: 20,
-//       dataList: this.dataList,
-//       task: this.task_failure,
-//     };
-//     const Megumi = new Megumi(params);
-//     Megumi.bite()
-//     .then( result => {
-//       assert(false);
-//       done();
-//     })
-//     .catch( err => {
-//       assert(err.message === TASK_ERROR_MESSAGE);
-//       assert(_.isError(err));
-//       done();
-//     });
-//   });
-
-//   // it('should return Promise.reject when dont pass constructor argument', (done) => {
-//   //   const Megumi = new Megumi();
-//   //   Megumi.bite()
-//   //   .then( result => {
-//   //     assert(false);
-//   //     done();
-//   //   })
-//   //   .catch( err => {
-//   //     assert(err.message === PARAMETER_IS_INVALID_ERROR_MESSAGE);
-//   //     assert(_.isError(err));
-//   //     done();
-//   //   });
-//   // });
-
-// });
+    this.megumi.retrieve(opts)
+    .then( tweets => {
+      assert(false);
+      done();
+    })
+    .catch( err => {
+      assert(err.message === NON_EXIST_USER_ERROR_MESSAGE);
+      done();
+    });
+  });
+});
